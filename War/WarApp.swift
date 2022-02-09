7d06a83323e477e9bb196a298ecf43d1d07dbdc7
Tylor Schafer <tylorschafer@gmail.com>
2022-02-09 13:03:58 -0700

    Add base UI to game

---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Tylor Schafer on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
